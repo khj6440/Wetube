a5193e1b946e3e1e583a3ed7e798071680d0f306
khj6440 <gurwls7673@gmail.com>
2019-05-17 19:38:18 +0900

    creating View

--- app.js
+++ app.js
@@ -7,13 +7,29 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 const app = express();
 
+app.use(helmet());
+app.set("view engine","pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(helmet());
 app.use(morgan("dev"));
+/*아래 코드와 같은뜻
+ app.use(function(req,res,next){
+
+ })
+-----------------------------
+app.use((req,res,next)=>{
+})
+*/
+ app.use(localsMiddleware);
+
+
+
+
+
 
 app.use(routes.home,globalRouter);
 app.use(routes.users,userRouter);
